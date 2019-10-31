f1b873eedbfd8882073d087a42c4f1b28454ee5a
Boris <boris@velodash.co>
2019-10-31 14:29:20 +0800

    remove unused function

--- lib/measure.dart
+++ lib/measure.dart
@@ -2,22 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-enum Unit { metric, imperial }
-
 class Measure {
   static const MethodChannel _channel = const MethodChannel('plugins.flutter.io/measure');
 
   static Future<bool> get isDistanceMetric async => await _channel.invokeMethod("distanceIsMetric");
 
   static Future<bool> get isHeightWeightMetric async => await _channel.invokeMethod("heightIsMetric");
-
-  static Future<Unit> getUnit() async {
-    final bool isMetric = await Measure.isDistanceMetric;
-    return (isMetric) ? Unit.metric : Unit.imperial;
-  }
-
-  static Future<Unit> getHeightWeightUnit() async {
-    final bool isMetric = await Measure.isHeightWeightMetric;
-    return (isMetric) ? Unit.metric : Unit.imperial;
-  }
 }
